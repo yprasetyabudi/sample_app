61338aeff6f5873675f0d67fd59d26e972982f8a
y prasetya budi <yprasetyabudi@gmail.com>
2013-03-29 18:04:25 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
   get "users/new"
 
-  #get "static_pages/home"
-  #get "static_pages/help"
-  #get "static_pages/about"
-  #get "static_pages/contact"    
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"    
   root to: 'static_pages#home'
   #match '/root',    to: 'static_pages#home'
   
